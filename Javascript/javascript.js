67a1748903f1e597a4285c96ee52b148b33e4ac6
Divya-Manivannan1 <reethudya@gmail.com>
2024-01-31 11:23:24 +0000

    Javascript practice

---
+++ Javascript/javascript.js
@@ -0,0 +1,30 @@
+console.log("Hello")
+
+let name="coach"
+if(name=="coach")
+    console.log("It is a coach")
+
+
+let greeting="bonjour"
+if(greeting=="bonjour")
+    console.log("comment ca va")
+
+let score=10
+
+if(score>=10)
+    console.log("You have won")
+
+
+let age=85
+if(age>65)
+    console.log("Time to retire")
+
+let hasCovid=false
+if(hasCovid)
+    console.log("Time to isolate")
+else
+    console.log("Keep social distancing")
+
+let isBritish=true
+if(age>18 && isBritish)
+    console.log("you can vote in the next election")
