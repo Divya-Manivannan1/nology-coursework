d946d667bd966b7076729d0585bc5538e471db23
Divya-Manivannan1 <reethudya@gmail.com>
2024-02-04 19:16:33 +0000

    Finished: Find the greatest number

--- Javascript/Greatest number/javascript.js
+++ Javascript/Greatest number/javascript.js
@@ -1,17 +1,37 @@
-function checkIfNumber(){
-
+function checkIfPositiveOrNegative(event){
+    const num=Number(event.target.value)
+    alert(num)
+    if(num != NaN){
+        if(num>0){
+            alert("It is a positive number")
+        }
+        else if(num<0){
+            alert("It is a negative number")
+        }
+        else{
+            alert("You have entered zero")
+        }
+    }else{
+        alert("Please enter a valid number")
+    }
 }
 
+function checkForGreatestNumber(event){
+    const num1 = number1.value
+    const num2 = number2.value
+    const num3 = number3.value
+    let gNum = num1
+    if(num2>gNum){
+        if(num3 > num2){
+            gNum = num3
+        }else{
+            gNum = num2
+        }
+    }else{
+        if(num3 > gNum){
+            gNum = num3
+        }
+    }
 
-function checkIfPositiveOrNegative(event){
-    num=event.target.value
-    if(num>0){
-        alert("It is a positive number")
-    }
-    else if(num==0){
-        alert("It is zero")
-    }
-    else{
-        alert("It is a negative number")
-    }
+    greatestNumber.value=gNum
 }
