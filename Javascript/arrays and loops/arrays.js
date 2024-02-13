e59b0b255316f0d265097972f79787227bd5c398
Divya-Manivannan1 <reethudya@gmail.com>
2024-02-13 11:44:39 +0000

    loops code along

--- Javascript/arrays and loops/arrays.js
+++ Javascript/arrays and loops/arrays.js
@@ -2,7 +2,7 @@ let coaches=["dave","sam", "andy"];
 const pets = ["cat", "dog", "parrot"];
 
 coaches[2]="mary";
-coaches[3]="dave";
+coaches[3]="andy";
 
 pets[2] = "hamster";
 pets[4]= "parrot";
@@ -10,3 +10,7 @@ console.log(coaches);
 console.log(coaches[1]);
 
 console.log(pets);
+
+for(let index=0; index<coaches.length; index++){
+    console.log(`Coach number ${index+1} is ${coaches[index]}`);
+}
