890064162825681c1f59cefc98d8a5c13e36a678
Divya-Manivannan1 <reethudya@gmail.com>
2024-02-13 11:37:37 +0000

    Arrays code along

---
+++ Javascript/arrays and loops/arrays.js
@@ -0,0 +1,12 @@
+let coaches=["dave","sam", "andy"];
+const pets = ["cat", "dog", "parrot"];
+
+coaches[2]="mary";
+coaches[3]="dave";
+
+pets[2] = "hamster";
+pets[4]= "parrot";
+console.log(coaches);
+console.log(coaches[1]);
+
+console.log(pets);
